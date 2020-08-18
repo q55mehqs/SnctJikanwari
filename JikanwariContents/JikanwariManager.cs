35d530d3e87cfc3b34d2b0310e6075a27ee17ced
q55mehqs <s1701057@sendai-nct.jp>
2020-08-18 14:40:08 +0900

    refactor parameter

--- JikanwariContents/JikanwariManager.cs
+++ JikanwariContents/JikanwariManager.cs
@@ -10,11 +10,11 @@ namespace SnctJikanwari.JikanwariContents
 {
     public static class JikanwariManager
     {
-        public static async Task<(List<IJugyo>, DayOfWeek)> GetJikanwari(string className, DateTime date, string cache = "",
-            DateTime? callDate = null)
+        public static async Task<(List<IJugyo>, DayOfWeek)> GetJikanwari(string className, DateTime date,
+            string cache = "")
         {
             var rawJikanwariData =
-                string.IsNullOrEmpty(cache) ? await LoadJikanwari(className, callDate) : cache;
+                string.IsNullOrEmpty(cache) ? await LoadJikanwari(className, date) : cache;
             var dailyJikanwari = DefaultJugyo.GetDailyJikanwari(className, date, rawJikanwariData, out var dayOfWeek)?
                 .Cast<IJugyo>().ToList();
 
@@ -48,7 +48,7 @@ namespace SnctJikanwari.JikanwariContents
             await using var stream =
                 assembly.GetManifestResourceStream("SnctJikanwari.DefaultJikanwari.versionStart.csv");
             if (stream == null) throw new Exception("version steam cannot open");
-            
+
             using var sr = new StreamReader(stream);
             var text = await sr.ReadToEndAsync();
             var lines = text.Split("\n").Select(t => t.Split(","));
@@ -72,7 +72,7 @@ namespace SnctJikanwari.JikanwariContents
 
         public static string JikanwariText(IReadOnlyList<IJugyo> jikanwari, bool isAs)
         {
-            var noneText = isAs ? "ASクラスの授業はこの曜日には開講されていません" : "該当日時に授業は設定されていません";
+            var noneText = isAs ? "ASクラスの授業はこの曜日には開講されていません 所属クラスの時間割も参照ください" : "該当日に授業は設定されていません";
             return jikanwari.Count == 0
                 ? noneText
                 : string.Join("\n\n", jikanwari.Select(jugyo => jugyo.ToString()));
